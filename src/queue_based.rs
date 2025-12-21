bc6fb245001630419249fa4ac634c50bc721d0a4
rosy <lc_rosy@qq.com>
2025-12-21 21:53:45 +0800

    更新接口

--- src/queue_based.rs
+++ src/queue_based.rs
@@ -1,31 +1,58 @@
-use crate::interface::AbsIPCEntity;
-use crate::vqueue::IPCItem;
+use core::ops::Deref;
+
+// use crate::interface::AbsIPCEntity;
+use crate::{
+    interface::{IPCSharedEntity, LocalEntityIf, SharedEntityIf},
+    vqueue::IPCItem,
+};
 use alloc::string::String;
 
-pub struct QueueBasedEntity {
+pub struct QueueBasedSharedEntity {
     queue_id: usize,
-    default_dispatcher: bool,
-    // todo
 }
 
-impl AbsIPCEntity for QueueBasedEntity {
-    fn unregister(&mut self) -> Result<(), String> {
-        todo!()
-    }
-
+impl SharedEntityIf for QueueBasedSharedEntity {
+    /// id的高8位需被保留，从而区分不同类型的IPC实体
     fn id(&self) -> u64 {
-        todo!()
+        self.queue_id as u64
     }
 
-    fn from_id(id: u64) -> Result<Self, String> {
-        todo!()
+    fn from_id(id: u64) -> Result<Self, String>
+    where
+        Self: Sized,
+    {
+        Ok(Self {
+            queue_id: id as usize,
+        })
     }
 
-    fn send_to_inner(&self, item: IPCItem) -> Result<(), String> {
+    /// 发送消息给self
+    fn send_to(&self, item: IPCItem) -> Result<(), String> {
         todo!()
     }
+}
 
+pub struct QueueBasedLocalEntity {
+    shared: IPCSharedEntity,
+    use_default_dispatcher: bool,
+}
+
+impl QueueBasedLocalEntity {
+    pub fn new() -> Self {
+        todo!()
+    }
+}
+
+impl LocalEntityIf for QueueBasedLocalEntity {
     async fn recv_inner(&self, msg_type: u64) -> Result<IPCItem, String> {
         todo!()
+    }
+}
+
+impl Deref for QueueBasedLocalEntity {
+    type Target = IPCSharedEntity;
+
+    fn deref(&self) -> &Self::Target {
+        &self.shared
     }
 }
--- src/sched_based.rs
+++ src/sched_based.rs
@@ -1,29 +1,51 @@
-use crate::interface::AbsIPCEntity;
-use crate::vqueue::IPCItem;
+// use crate::interface::AbsIPCEntity;
+use crate::{interface::SharedEntityIf, vqueue::IPCItem};
 use alloc::string::String;
 
-pub struct SchedBasedEntity {
+pub struct SchedBasedSharedEntity {
     // todo
 }
 
-impl AbsIPCEntity for SchedBasedEntity {
-    fn unregister(&mut self) -> Result<(), String> {
-        todo!()
-    }
+// impl AbsIPCEntity for SchedBasedEntity {
+//     fn unregister(&mut self) -> Result<(), String> {
+//         todo!()
+//     }
 
+//     fn id(&self) -> u64 {
+//         todo!()
+//     }
+
+//     fn from_id(id: u64) -> Result<Self, String> {
+//         todo!()
+//     }
+
+//     fn send_to_inner(&self, item: IPCItem) -> Result<(), String> {
+//         todo!()
+//     }
+
+//     async fn recv_inner(&self, msg_type: u64) -> Result<IPCItem, String> {
+//         todo!()
+//     }
+// }
+
+impl SharedEntityIf for SchedBasedSharedEntity {
+    /// id的高8位需被保留，从而区分不同类型的IPC实体
     fn id(&self) -> u64 {
-        todo!()
+        // todo
+        0
     }
 
-    fn from_id(id: u64) -> Result<Self, String> {
-        todo!()
+    fn from_id(id: u64) -> Result<Self, String>
+    where
+        Self: Sized,
+    {
+        // todo
+        Err(String::from("Not implemented"))
     }
 
-    fn send_to_inner(&self, item: IPCItem) -> Result<(), String> {
-        todo!()
-    }
-
-    async fn recv_inner(&self, msg_type: u64) -> Result<IPCItem, String> {
-        todo!()
+    /// 发送消息给self
+    fn send_to(&self, item: IPCItem) -> Result<(), String> {
+        // todo
+        Err(String::from("Not implemented"))
     }
 }
